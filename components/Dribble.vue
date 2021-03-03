ed42e4c4cee0d4e097496aa2e5ab0d4e133a585e
melvnl <melvinlim2000@gmail.com>
2021-03-03 16:54:26 +0700

    Add/change images

--- components/Dribble.vue
+++ components/Dribble.vue
@@ -7,14 +7,11 @@
       <a href="https://dribbble.com/bymelvn" class=" text-sm font-medium  p-2 bg-backgroundColor rounded-md hover:bg-backgroundHoverColor">View More</a>
     </div>
     <div class=" mt-5 md:p-0 grid grid-cols-1 gap-1 md:grid-cols-2">
-      <a href="https://www.youtube.com/watch?v=IHNzOHi8sJs" class="hover:opacity-50">
+      <a href="https://dribbble.com/bymelvn" class="hover:opacity-50">
         <img src="../assets/images/works/dribble/1.png" class="m-0 rounded-md" alt="Indonesia Patisserie School">
       </a>
-      <a href="https://www.youtube.com/watch?v=IHNzOHi8sJs" class="hover:opacity-50">
-        <img src="../assets/images/works/test.jpeg" class="m-0 rounded-md" alt="Julizar">
-      </a>
-      <a href="https://www.youtube.com/watch?v=IHNzOHi8sJs" class="hover:opacity-50">
-        <img src="../assets/images/works/test.jpeg" class="m-0 rounded-md" alt="">
+      <a href="https://dribbble.com/bymelvn" class="hover:opacity-50">
+        <img src="../assets/images/works/dribble/2.png" class="m-0 rounded-md" alt="Julizar">
       </a>
     </div>
   </div>

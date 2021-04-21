c8fbb70f978c71eab01da5f6dc6bfe3daea5006a
himelv <63998835+melvnl@users.noreply.github.com>
2021-04-22 01:54:31 +0700

    fix overlap footer img

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="mt-4 mb-2 p-2 flex flex-row justify-between md:p-0">
     <div class="flex">
-      <a href="https://github.com/melvnl" aria-label="Github"><img src="../assets/images/icons/github.svg" alt="Github Icon"></a>
-      <a href="https://www.linkedin.com/in/melvin-liu/" aria-label="Linkedin"><img src="../assets/images/icons/linkedin.svg" alt="Linkedin Icon" class="mx-1"></a>
+      <a href="https://github.com/melvnl" aria-label="Github" class="mr-2"><img src="../assets/images/icons/github.svg" alt="Github Icon"></a>
+      <a href="https://www.linkedin.com/in/melvin-liu/" aria-label="Linkedin" class="mx-2"><img src="../assets/images/icons/linkedin.svg" alt="Linkedin Icon"></a>
     </div>
     <div class=" flex font-semibold">
       Made with ☕ by Melvin

6d8c0e7d905a9ffe1e3e3c8230124acb690eba30
himelv <63998835+melvnl@users.noreply.github.com>
2021-10-16 19:42:59 +0700

    [MODIFY] Add & modify trusted logo row

--- components/Trusted.vue
+++ components/Trusted.vue
@@ -3,7 +3,10 @@
     <h1 class="text-2xl font-bold">
       Trusted By
     </h1>
-    <div class=" mt-5 md:p-0 flex flex-row items-center">
+    <div class=" mt-5 md:p-0 flex flex-col md:flex-row items-start md:items-center">
+      <div class="">
+        <img src="../assets/images/works/trusted/bukalapak.png" class="mx-2" alt="Bukalapak">
+      </div>
       <img src="../assets/images/works/trusted/tanifund.png" class="mx-2" alt="Tanifund">
       <img src="../assets/images/works/trusted/skilvul.png" class="mx-2" alt="Skilvul">
     </div>
